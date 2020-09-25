eb3836af945978ac9ece8e99c4c3d029f77d746f
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-09-25 08:14:38 +0200

    Cart functionality init

--- project-pizzeria/src/js/functions.js
+++ project-pizzeria/src/js/functions.js
@@ -1,4 +1,4 @@
-/* global Handlebars */
+/* global Handlebars, dataSource */
 
 const utils = {}; // eslint-disable-line no-unused-vars
 
@@ -6,7 +6,6 @@ utils.createDOMFromHTML = function(htmlString) {
   let div = document.createElement('div');
   div.innerHTML = htmlString.trim();
   return div.firstChild;
-
 };
 
 utils.createPropIfUndefined = function(obj, key, value = []){
@@ -37,6 +36,19 @@ utils.serializeFormToObject = function(form){
   return output;
 };
 
+utils.convertDataSourceToDbJson = function(){
+  const productJson = [];
+  for(let key in dataSource.products){
+    productJson.push(Object.assign({id: key}, dataSource.products[key]));
+  }
+
+  console.log(JSON.stringify({product: productJson, order: []}, null, '  '));
+};
+
 Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
   return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+
+Handlebars.registerHelper('joinValues', function(input, options) {
+  return Object.values(input).join(options.fn(this));
 });
