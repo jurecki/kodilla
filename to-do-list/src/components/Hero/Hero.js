f22309239325ffd15a02d4b409b3d38fb46faf0f
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-11-16 15:17:09 +0100

    Project init

---
+++ to-do-list/src/components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+const Hero = () => {
+    return (
+        <section>
+            <h2>Things to do</h2>
+            <img src='../../img/space-big.png'/>
+        </section>
+    )
+}
+
+export default Hero;
---
+++ to-do-list/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/normalize.scss';
+import './styles/global.scss';
+import App from './components/App/App';
+
+ReactDOM.render(<App/>, document.getElementById('app'));  
